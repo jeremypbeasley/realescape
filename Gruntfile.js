d8f24f7ae11c039e6b6c5ba720d4790767986db3
Jeremy P. Beasley <jbeasley@starbucks.com>
2017-10-25 00:09:49 -0700

    fixing js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,50 @@
 module.exports = function(grunt) {
-
-    // 1. All configuration goes here
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      stylus: {
-          options: {
-              'compress': false,
-              'include css': true
-          },
-          compile: {
-              files: {
-                'assets/master.css': '_resources/_styles/master.styl',
-              }
-          }
+  // 1. All configuration goes here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    stylus: {
+      options: {
+        'compress': false,
+        'include css': true
       },
-      concat: {
-          dist: {
-              src: [
-                  'node_modules/jquery/dist/jquery.min.js',
-                  'node_modules/velocity/velocity.js',
-                  'node_modules/velocity/velocity.ui.js',
-                  '_resources/_scripts/main.js'
-              ],
-              dest: 'assets/master.js',
-          }
-      },
-      watch: {
-          scripts: {
-              files: ['_resources/_scripts/*.js', 'Gruntfile.js', '_resources/_styles/*.styl'],
-              tasks: ['concat', 'stylus'],
-              options: {
-                  spawn: false
-              },
-          }
-      },
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'stylus', 'watch' ]);
-
+      compile: {
+        files: {
+          'master.css': '_resources/_styles/master.styl',
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          'node_modules/jquery/dist/jquery.min.js',
+          'bower_components/hammerjs/hammer.js',
+          'bower_components/jquery.hammer.js/jquery.hammer.js',
+          'bower_components/velocity/velocity.js',
+          'bower_components/velocity/velocity.ui.js',
+          '_resources/_scripts/main.js',
+          '_resources/_scripts/main.js'
+        ],
+        dest: 'master.js',
+      }
+    },
+    watch: {
+      scripts: {
+        files: [
+          '_resources/_scripts/*.js',
+          'Gruntfile.js',
+          '_resources/_styles/*.styl'
+        ],
+        tasks: ['concat', 'stylus'],
+        options: {
+          spawn: false
+        },
+      }
+    },
+  });
+  // 2. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  // 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', ['concat', 'stylus', 'watch']);
 };
